8672fccab09a02e10755dfbc69d62a1f0b804e93
Edwin Ebube <edwinjosh37@gmail.com>
2025-01-18 07:35:16 +0100

    updated code

--- Admin/Admin/vite.config.js
+++ Admin/Admin/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Kings_Store/", // Add this to match your repository name
   server: {
     proxy: {
       "/api": {
--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:3000"
-      }
-    }
-  }
-})
+  base: "/Kings_Store/", // Add this to match your repository name
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
+});
